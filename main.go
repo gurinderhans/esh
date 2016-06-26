b912630cdad4273935c7a53a1c5413d1aed36f5d
Gurinder Hans <hello@gurinderhans.me>
2016-06-25 21:30:18 -0700

    Add basic ssh code to get server list

--- main.go
+++ main.go
@@ -2,8 +2,51 @@ package main
 
 import (
 	"fmt"
+	"bytes"
+	"golang.org/x/crypto/ssh"
 )
+
+type ESHConfig struct {
+	Hostname string
+	Port string
+	Username string
+	Password string
+	Key string
+}
+
+func MakeSession(esh_conf *ESHConfig) (session *ssh.Session, err error) {
+	
+	config := &ssh.ClientConfig{
+		User: esh_conf.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(esh_conf.Password),
+		},
+	}
+
+	conn, err := ssh.Dial("tcp", esh_conf.Hostname + ":" + esh_conf.Port, config)
+
+	if err != nil {
+		return
+	}
+
+	session, err = conn.NewSession()
+
+	return
+}
 
 func main() {
 	fmt.Println("hello, lets go!")
+
+	session, err := MakeSession(&ESHConfig{Hostname: "192.168.0.11", Port: "22", Username: "pi", Password: "raspberry"})
+	defer session.Close()
+
+	if err != nil {
+		panic("Error: " + err.Error())
+		return
+	}
+	var stdoutBuf bytes.Buffer
+    session.Stdout = &stdoutBuf
+    session.Run("ls")
+
+    fmt.Println(stdoutBuf.String())
 }
