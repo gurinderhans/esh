1001d6775b4fa1bd6f8069f59836539e7ff8cc56
Gurinder Hans <hello@gurinderhans.me>
2016-06-26 00:14:29 -0700

    able to run basic commands on ssh device

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"bytes"
+	"strings"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -14,12 +16,12 @@ type ESHConfig struct {
 	Key string
 }
 
-func MakeSession(esh_conf *ESHConfig) (session *ssh.Session, err error) {
+func MakeSession (esh_conf *ESHConfig) (session *ssh.Session, err error) {
 	
 	config := &ssh.ClientConfig{
 		User: esh_conf.Username,
 		Auth: []ssh.AuthMethod{
-			ssh.Password(esh_conf.Password),
+			ssh.Password(esh_conf.Password), // TODO: deal with key
 		},
 	}
 
@@ -34,9 +36,7 @@ func MakeSession(esh_conf *ESHConfig) (session *ssh.Session, err error) {
 	return
 }
 
-func main() {
-	fmt.Println("hello, lets go!")
-
+func Command (cmd string) {
 	session, err := MakeSession(&ESHConfig{Hostname: "192.168.0.11", Port: "22", Username: "pi", Password: "raspberry"})
 	defer session.Close()
 
@@ -44,9 +44,28 @@ func main() {
 		panic("Error: " + err.Error())
 		return
 	}
+
 	var stdoutBuf bytes.Buffer
     session.Stdout = &stdoutBuf
-    session.Run("ls")
+    session.Run(cmd)
 
     fmt.Println(stdoutBuf.String())
+}
+
+func ParseArgs (args []string) {
+
+	if args[1] == "-n" {
+		// args[2] will contain some ssh session name
+		// args[3..n] will be the command
+	} else if (args[1] == "list-all") {
+		// list all saved ssh sessions
+	} else {
+		// args[1..n] is the command, because a session has been selected
+		cmd := strings.Join(args[1:], " ")
+		Command(cmd)
+	}
+}
+
+func main() {
+	ParseArgs(os.Args)
 }
